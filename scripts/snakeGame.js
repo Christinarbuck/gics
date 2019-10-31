7de25c02eb3e825b0041beb129e174a6178eff8a
kingoftheconnors <Connormcp777@gmail.com>
2019-10-31 16:19:14 -0400

    Decreased speed-up rate and start games with two foods

--- scripts/snakeGame.js
+++ scripts/snakeGame.js
@@ -16,7 +16,7 @@ export default class SnakeGame
 			S: 83,
 			D: 68
 		};
-		this.backgroundColor = "lightblue";
+		this.backgroundColor = "rgb(0, 162, 232)";
 		this.gameSpeed = 28;
 		this.score = [];
 		this.BLOCK_SIZE = 40;
@@ -33,10 +33,10 @@ export default class SnakeGame
 	initialize()
 	{
 		
-		const SNAKE_COLOUR_1 = 'darkseagreen';
-		const SNAKE_BORDER_COLOUR_1 = 'green';
-		const SNAKE_COLOUR_2 = 'coral';
-		const SNAKE_BORDER_COLOUR_2 = 'brown';
+		const SNAKE_COLOUR_1 = 'rgb(181, 230, 29)';
+		const SNAKE_BORDER_COLOUR_1 = 'rgba(103, 132, 15)';
+		const SNAKE_COLOUR_2 = 'rgb(236, 119, 89)';
+		const SNAKE_BORDER_COLOUR_2 = 'rgb(208, 73, 34)';
 			
 		this.player1 = new Snake(1, 400, 40, this);
 		this.player1.setBlockColor(SNAKE_COLOUR_1);
@@ -49,15 +49,16 @@ export default class SnakeGame
 		this.score.push(0)
 
 		this.food = new Food(this);
+		this.food2 = new Food(this);
 
 		new InputHandler(this.player1, this.KEYS.LEFT_ARROW, this.KEYS.RIGHT_ARROW, 
 			this.KEYS.UP_ARROW, this.KEYS.DOWN_ARROW);
 		new InputHandler(this.player2, this.KEYS.A, this.KEYS.D, 
 			this.KEYS.W, this.KEYS.S);
 
-		this.gameObjects.push(this.player1, this.player2, this.food);
+		this.gameObjects.push(this.player1, this.player2, this.food, this.food2);
 		this.snakeObjects.push(this.player1, this.player2);
-		this.foodObjects.push(this.food);
+		this.foodObjects.push(this.food, this.food2);
 	}// end method
 
 	getGameWidth() { return this.gameWidth; }
@@ -74,7 +75,7 @@ export default class SnakeGame
 		return total;
 	}
 
-	getGameSpeed() { return this.gameSpeed - this.getTotalScore(); }
+	getGameSpeed() { return this.gameSpeed - this.getTotalScore()/2; }
 
 	increaseScore(snakeID) {
 		this.score[snakeID-1]++;
@@ -117,8 +118,9 @@ export default class SnakeGame
 		this.player1.reset(this.BLOCK_SIZE, this.BLOCK_SIZE*3);
 		this.player2.reset(this.gameWidth - this.BLOCK_SIZE*4, this.BLOCK_SIZE*3);
 		this.food.respawn();
-		this.gameObjects = [this.player1, this.player2, this.food];
-		this.foodObjects = [this.food];
+		this.food2.respawn();
+		this.gameObjects = [this.player1, this.player2, this.food, this.food2];
+		this.foodObjects = [this.food, this.food2];
 		this.gameOvered = false;
 	}
 
